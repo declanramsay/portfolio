2a24f16340ffd918d0c1d72772dd5e0984579789
Declan Ramsay <declandr@gmail.com>
2017-02-08 00:39:19 +1300

    Add sketch of Zombies Run API integration

--- app/components/social-list.js
+++ app/components/social-list.js
@@ -10,6 +10,7 @@ const {
 export default Component.extend({
   tracks: computed('recentTracks', function() {
     let { track } = get(this, 'recentTracks');
+
     return track.map((t, i) => {
       let artist = get(t, 'artist.#text');
       let isLast = i + 1 === track.length;
@@ -19,5 +20,14 @@ export default Component.extend({
         isLast,
       };
     });
+  }),
+
+  latestRun: computed('run', function() {
+    let { metrics, summary: { first_mission: missionCode }, id } = get(this, 'run');
+    let time = Math.round((metrics.duration / 60) * 100) / 100;
+    let distance = Math.round((metrics.distance / 1000) * 100) / 100;
+    let link = `https://zombiesrungame.com/zombielink/runs-detailed/${id}/public/`;
+
+    return { time, distance, missionCode, link };
   }),
 });
--- app/routes/home.js
+++ app/routes/home.js
@@ -12,6 +12,9 @@ const {
 const LASTFM_KEY = '45c2ed86bea3e8d841b7d430ba0e605e';
 const LASTFM_ENDPOINT = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=2&user=lucideffective&api_key=${LASTFM_KEY}&format=json`;
 
+// Need a latest-run endpoint?
+const ZOMBIESRUN_ENDPOINT = 'https://api.zombiesrungame.com/runs/11352204/';
+
 export default Route.extend({
   api: service(),
 
@@ -19,8 +22,8 @@ export default Route.extend({
     let api = get(this, 'api');
 
     let hashOfPromises = {
-      recentTracks: api.fetch(LASTFM_ENDPOINT)
-        .then((response) => response.recenttracks),
+      recentTracks: api.fetch(LASTFM_ENDPOINT).then((response) => response.recenttracks),
+      run: api.fetch(ZOMBIESRUN_ENDPOINT).then((response) => response),
     };
 
     return hash(hashOfPromises);
