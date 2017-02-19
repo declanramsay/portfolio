f62b305cdc17e3452ea953fa7f81e0a14d3cff69
Declan Ramsay <declandr@gmail.com>
2017-02-20 01:21:27 +1300

    Add content for Tour, initial implementation of html for extra fields in single-project

--- app/routes/home.js
+++ app/routes/home.js
@@ -49,10 +49,42 @@ const PROJECTS_DATA = [
       src: '../assets/images/national-tour-auckland.jpg',
       alt: 'National Tour',
     },
-    description: '',
+    description: '<p>In 2016, after the release of my single <i>Gretchen Ross</i> I embarked on a solo tour of New Zealand, with fellow musician Emily Riordan in support. Spread over 5 cities, we performed to hundreds of people and travelled all the way from Auckland to Dunedin.</p> <p>Despite being our debut tour, our first time performing in the South Island, and being entirely self-organised we were very successful and broke even by tour\'s end.</p>',
+    links: [
+      {
+        title: 'Second Hand News',
+        url: 'http://www.secondhandnews.nz/news--reviews/lucid-effect-national-tour',
+      },
+      {
+        title: '13th Floor',
+        url: 'https://www.13thfloor.co.nz/?p=71593',
+      },
+      {
+        title: 'Muzic.net.nz',
+        url: 'http://www.muzic.net.nz/news/5619/lucid-effect-announces-national-tour.html',
+      },
+      {
+        title: 'Taranaki Daily News',
+        url: 'http://www.stuff.co.nz/taranaki-daily-news/news/82717344/new-plymouth-musician-lucid-effect-sets-sail-for-national-tour',
+      },
+    ],
     tags: ['Event Planning', 'Design'],
-    date: '',
-    images: [],
+    date: 'August 2016',
+    images: [
+      {
+        src: '../assets/images/national-poster.jpg',
+        alt: 'National Tour',
+      },
+      {
+        src: '../assets/images/national-tour-performing.jpg',
+        alt: 'National Tour',
+      },
+      {
+        src: '../assets/images/national-tour-lead.jpg',
+        alt: 'National Tour',
+      },
+    ],
+    skills: ['Management', 'Planning', 'Budgeting', 'Promotion', 'Advertising', 'Performing', 'Creativity', 'Press'],
   },
   {
     id: 'what-are-you-voting-for',
--- app/routes/project.js
+++ app/routes/project.js
@@ -15,6 +15,27 @@ const MOCK_ITEM = {
     src: '../assets/images/national-tour-auckland.jpg',
     alt: 'National Tour',
   },
+  description: '<p>In 2016, after the release of my single <i>Gretchen Ross</i> I embarked on a solo tour of New Zealand, with fellow musician Emily Riordan in support. Spread over 5 cities, we performed to hundreds of people and travelled all the way from Auckland to Dunedin.</p> <p>Despite being our debut tour, our first time performing in the South Island, and being entirely self-organised we were very successful and broke even by tour\'s end.</p>',
+  links: [
+    {
+      title: 'Second Hand News',
+      url: 'http://www.secondhandnews.nz/news--reviews/lucid-effect-national-tour',
+    },
+    {
+      title: '13th Floor',
+      url: 'https://www.13thfloor.co.nz/?p=71593',
+    },
+    {
+      title: 'Muzic.net.nz',
+      url: 'http://www.muzic.net.nz/news/5619/lucid-effect-announces-national-tour.html',
+    },
+    {
+      title: 'Taranaki Daily News',
+      url: 'http://www.stuff.co.nz/taranaki-daily-news/news/82717344/new-plymouth-musician-lucid-effect-sets-sail-for-national-tour',
+    },
+  ],
+  tags: ['Event Planning', 'Design'],
+  date: 'August 2016',
   images: [
     {
       src: '../assets/images/national-poster.jpg',
@@ -29,9 +50,7 @@ const MOCK_ITEM = {
       alt: 'National Tour',
     },
   ],
-  description: '',
-  tags: ['Event Planning', 'Design'],
-  date: 'August 2016',
+  skills: ['Management', 'Planning', 'Budgeting', 'Promotion', 'Advertising', 'Performing', 'Creativity', 'Press'],
 };
 
 export default Route.extend({
@@ -39,5 +58,10 @@ export default Route.extend({
     return hash({
       project: resolve(MOCK_ITEM),
     });
+  },
+
+  activate() {
+    this._super();
+    window.scrollTo(0, 0);
   },
 });
