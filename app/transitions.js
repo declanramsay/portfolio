45d55b6446bb1900daa57830d9822d040c7fc15e
Declan Ramsay <declandr@gmail.com>
2017-03-05 11:34:54 +1300

    Make adjustments to size of project hover text

--- app/transitions.js
+++ app/transitions.js
@@ -1,13 +1,17 @@
 export default function() {
+  // this.transition(
+  //   this.fromRoute('home'),
+  //   this.use('explode', {
+  //     matchBy: 'data-image-id',
+  //     use: ['fly-to', { duration: 500 }],
+  //     // use: ['fly-to', { duration: 1000, easing: 'easeOutQuart' }],
+  //   }, {
+  //     use: ['fade', { duration: 500 }],
+  //   }),
+  //   this.reverse('fade')
+  // );
+
   this.transition(
-    this.fromRoute('home'),
-    this.use('explode', {
-      matchBy: 'data-image-id',
-      use: ['fly-to', { duration: 500 }],
-      // use: ['fly-to', { duration: 1000, easing: 'easeOutQuart' }],
-    }, {
-      use: ['fade', { duration: 500 }],
-    }),
-    this.reverse('fade')
+    this.use('fade')
   );
 }
