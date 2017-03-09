7946775f25cc159f1af6cb065af927ff817c303d
Declan <declan@alphero.com>
2017-03-09 13:39:50 +1300

    Shift social calls to component

--- app/components/social-list.js
+++ app/components/social-list.js
@@ -4,16 +4,23 @@ const {
   Component,
   computed,
   get,
+  inject: { service },
+  RSVP: { hash },
+  setProperties,
 } = Ember;
 
 export default Component.extend({
-  // attrs
-  run: null,
-  reading: null,
-  recentTracks: null,
+  social: service(),
+  lastfm: null,
+  zombies: null,
 
-  tracks: computed('recentTracks', function() {
-    let { track } = get(this, 'recentTracks');
+  tracks: computed('lastfm', function() {
+    let lastfm = get(this, 'lastfm');
+    if(!lastfm) {
+      return;
+    }
+
+    let { track } = lastfm;
 
     return track.map((t, i) => {
       let artist = get(t, 'artist.#text');
@@ -26,12 +33,42 @@ export default Component.extend({
     });
   }),
 
-  latestRun: computed('run', function() {
-    let { metrics, summary: { first_mission: missionCode }, id } = get(this, 'run');
+  latestRun: computed('zombies', function() {
+    let zombies = get(this, 'zombies');
+    if(!zombies) {
+      return;
+    }
+
+    let { metrics, summary: { first_mission: missionCode }, id } = zombies;
     let time = Math.round((metrics.duration / 60) * 100) / 100;
     let distance = Math.round((metrics.distance / 1000) * 100) / 100;
     let link = `https://zombiesrungame.com/zombielink/runs-detailed/${id}/public/`;
 
     return { time, distance, missionCode, link };
   }),
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    let s = get(this, 'social');
+
+    let hashOfPromises = hash({
+      zombies: s.fetch('zombies'),
+      lastfm: s.fetch('lastfm'),
+    });
+
+    return hashOfPromises
+      .then(({ zombies, lastfm }) => {
+        if(!lastfm || !lastfm.recenttracks) {
+          lastfm = {};
+        }
+
+        let recentTracks = lastfm.recenttracks;
+
+        setProperties(this, {
+          zombies,
+          lastfm: recentTracks,
+        });
+      });
+  },
 });
--- app/routes/home.js
+++ app/routes/home.js
@@ -7,12 +7,6 @@ const {
   RSVP: { hash },
 } = Ember;
 
-// TODO tidy
-const LASTFM_KEY = '45c2ed86bea3e8d841b7d430ba0e605e';
-const LASTFM_ENDPOINT = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=3&user=lucideffective&api_key=${LASTFM_KEY}&format=json`;
-// Need a latest-run endpoint?
-const ZOMBIESRUN_ENDPOINT = 'https://api.zombiesrungame.com/runs/11352204/';
-
 const PROJECT_ENDPOINT = 'https://personal-portfolio-b7670.firebaseio.com/projects.json';
 
 export default Route.extend({
@@ -23,10 +17,6 @@ export default Route.extend({
 
     let hashOfPromises = {
       projects: api.fetch(PROJECT_ENDPOINT).then((response) => response),
-
-      // TODO move social-calls to service (don't hold up site for)
-      recentTracks: api.fetch(LASTFM_ENDPOINT).then((response) => response.recenttracks),
-      run: api.fetch(ZOMBIESRUN_ENDPOINT).then((response) => response),
     };
 
     return hash(hashOfPromises);
