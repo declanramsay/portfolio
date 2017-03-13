0f447be7bd628497b796e5c31855eb8b8bd1e7c6
Declan Ramsay <declandr@gmail.com>
2017-03-13 23:37:19 +1300

    Add Ember Fastboot

--- app/components/global/header-.js
+++ app/components/global/header-.js
@@ -1,17 +1,28 @@
 import Ember from 'ember';
-import Headroom from 'headroom';
 
 const {
   Component,
+  get,
+  inject: { service },
 } = Ember;
 
 export default Component.extend({
   tagName: 'div',
   classNames: ['container'],
+  fastboot: service(),
+
   didInsertElement() {
+    let isFastBoot = get(this, 'fastboot.isFastBoot');
+
+    if(isFastBoot) {
+      return;
+    }
+
     let header = this.$('.Header--headroom').get(0);
 
-    let headroom  = new Headroom(header, {
+    /* eslint-disable */
+    let headroom = new Headroom(header, {
+    /* eslint-enable */
       offset: 220,
     });
 
