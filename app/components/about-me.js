f29ef25859d7f5b552e7361f981ad45cea4fd82d
Declan Ramsay <declandr@gmail.com>
2017-03-23 21:14:11 +1300

    Remove descriptions for skills, flip

--- app/components/about-me.js
+++ app/components/about-me.js
@@ -2,56 +2,8 @@ import Ember from 'ember';
 
 const {
   Component,
-  set,
 } = Ember;
 
-const skills = [
-  {
-    label: 'HTML 5',
-    isActive: false,
-    description: 'Including Web Storage, File API, and <video>',
-  },
-  {
-    label: 'CSS 3',
-    isActive: false,
-  },
-  {
-    label: 'SASS',
-    isActive: false,
-  },
-  {
-    label: 'JavaScript',
-    isActive: false,
-    description: 'Including ES2016 ',
-  },
-  {
-    label: 'Ember',
-    isActive: false,
-  },
-  // {
-  //   label: 'NodeJS',
-  //   isActive: false,
-  // },
-  {
-    label: 'Responsive Web',
-    isActive: false,
-  },
-  {
-    label: 'Git SCM',
-    isActive: false,
-  },
-  {
-    label: 'Agile',
-    isActive: false,
-  },
-];
-
 export default Component.extend({
-  skills,
-
-  actions: {
-    toggleSkill(skill) {
-      set(skill, 'isActive', !skill.isActive);
-    },
-  },
+  skills: ['HTML 5', 'CSS3 + SASS', 'JavaScript', 'Ember', 'Responsive Web', 'Git SCM', 'Agile', 'TDD'],
 });
