77ad2873ee075168921947d277af91d43fb2d8db
Declan Ramsay <declandr@gmail.com>
2017-09-03 13:05:03 +0100

    Remove loading screen

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,32 +1,7 @@
 import Ember from 'ember';
-import injectService from 'ember-service/inject';
-import { task } from 'ember-concurrency';
 
 const {
   Route,
-  get,
-  $: $$,
 } = Ember;
 
-export default Route.extend({
-  fastboot: injectService(),
-
-  afterModel(model, transition) {
-    this._super(model, transition);
-    let isFastboot = get(this, 'fastboot.isFastBoot');
-    if(isFastboot) {
-      return;
-    }
-    get(this, 'removeAppLoader').perform();
-  },
-
-  /* eslint-disable */
-  removeAppLoader: task(function* () {
-    let bal = $$('#before-app-loader');
-    if(!bal.length) {
-      return;
-    }
-    bal.remove();
-  }),
-  /* eslint-enable */
-});
+export default Route.extend({});
